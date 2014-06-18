00eb97d396a5c36e8585430cbef4009b43708918
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-06-18 20:58:59 +0100

    Implement a Stack class

---
+++ lib/postfix/stack.rb
@@ -0,0 +1,46 @@
+module Postfix
+  # An implementation of a numeric-only Stack
+  class Stack
+    def initialize
+      @stack = []
+    end
+
+    # Pushes an item onto the Stack
+    #
+    # @param item [Numeric] item to be pushed onto the Stack
+    # @return [Array] the Stack as an Array (so it is reversed)
+    # @raise [ArgumentError] raises error if item is not numeric
+    def push(item)
+      fail ArgumentError, 'Item not numeric' unless item.is_a? Numeric
+      @stack.push(item)
+    end
+
+    # Removes the first item on the Stack
+    #
+    # @return [Numeric] the removed item from the Stack
+    def pop
+      @stack.pop
+    end
+
+    # Returns the first item on the Stack
+    #
+    # @return [Numeric] the first item on the Stack
+    def peek
+      @stack.last
+    end
+
+    # Returns the size of the Stack
+    #
+    # @return [Integer] the size of the Stack
+    def size
+      @stack.size
+    end
+
+    # Returns the Boolean value of whether or not the Stack has no items
+    #
+    # @return [Boolean]
+    def empty?
+      @stack.empty?
+    end
+  end
+end
---
+++ spec/postfix_spec/stack_spec.rb
@@ -0,0 +1,63 @@
+describe Postfix::Stack do
+  describe '#push' do
+    let(:stack) { Postfix::Stack.new }
+
+    it 'pushes a numeric item to the top of the Stack' do
+      expect { stack.push('1') }.to raise_error ArgumentError
+
+      stack.push(1)
+      expect(stack.peek).to eq 1
+
+      stack.push(2)
+      expect(stack.peek).to eq 2
+
+      stack.push(3)
+      expect(stack.peek).to eq 3
+    end
+  end
+
+  describe '#pop' do
+    let(:stack) { Postfix::Stack.new }
+
+    it 'pops the last item from the Stack' do
+      stack.push(1)
+      stack.push(2)
+
+      expect(stack.pop).to eq 2
+      expect(stack.peek).to eq 1
+    end
+  end
+
+  describe '#peek' do
+    let(:stack) { Postfix::Stack.new }
+
+    it 'returns the last item from the Stack' do
+      stack.push(1)
+      stack.push(2)
+      expect(stack.peek).to eq 2
+    end
+  end
+
+  describe '#size' do
+    let(:stack) { Postfix::Stack.new }
+
+    it 'calculates the size of the Stack' do
+      stack.push(1)
+      stack.push(2)
+
+      expect(stack.size).to eq 2
+    end
+  end
+
+  describe '#empty?' do
+    let(:stack) { Postfix::Stack.new }
+
+    it 'checks whether the Stack is empty' do
+      expect(stack.empty?).to eq true
+
+      stack.push(1)
+
+      expect(stack.empty?).to eq false
+    end
+  end
+end
