590324a54225e9b0eaae5a2c5a510189d0f68dd3
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-06-19 17:35:57 +0100

    Implement a parser

--- lib/postfix/error.rb
+++ lib/postfix/error.rb
@@ -1,4 +1,10 @@
 module Postfix
   # Raised when invalid token during tokenization is encountered
   class InvalidToken < StandardError; end
+
+  # Raised when operator doesn't have enough operands to work with
+  class InsufficientValuesInExpression < StandardError; end
+
+  # Raised when expression has 2+ operands but no operators
+  class InvalidExpression < StandardError; end
 end
---
+++ lib/postfix/parser.rb
@@ -0,0 +1,34 @@
+module Postfix
+  # An implementation of a reverse Polish notation parser
+  class Parser
+    attr_reader :tokens, :stack
+
+    # Initializes an empty Stack and tokenizes input
+    #
+    # @param string [String] string to be tokenized
+    def initialize(string)
+      @tokens = Tokenizer.new
+      @tokens = @tokens.tokenize(string)
+      @stack = Stack.new
+    end
+
+    # Evalutes the tokenized objects
+    #
+    # @return [Numeric] the numerical result
+    # @raise [InsufficientValuesInExpression]
+    # @raise [InvalidExpression]
+    def evaluate
+      @tokens.each do |token|
+        if token.type == :operand
+          @stack << token.value
+        elsif token.type == :operator
+          fail InsufficientValuesInExpression if @stack.size < 2
+          @stack << @stack.pop(2).reduce(token.value)
+        end
+      end
+
+      fail InvalidExpression if @stack.size > 1
+      return @stack.peek if @stack.size == 1
+    end
+  end
+end
