69bd86b3fb47de2a67c11649f4b6111c33e0e84d
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-06-20 15:40:20 +0100

    Add documentation for 'Postfix#setup'

--- lib/postfix.rb
+++ lib/postfix.rb
@@ -8,6 +8,10 @@ require 'postfix/parser'
 # An implementation of the reverse Polish notation
 module Postfix
   class << self
+    # Sets up helper instance method for String class
+    #
+    # @return [false, Symbol] false if method already defined,
+    #                         otherwise method Symbol
     def setup
       return false if String.respond_to?(:postfix)
       String.send(:define_method, :postfix) { Parser.new(self).evaluate }
